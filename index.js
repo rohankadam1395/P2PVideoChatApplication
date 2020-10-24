9d9dcd892ae12c74e8b9bbf61282a6de84260f04
Rohan Kadam <kadamrohan76@gmail.com>
2020-10-24 09:35:06 +0530

    changed / to *

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ var socket=require('socket.io');
 let port=process.env.PORT | 5000;
 
 
-app.get('/',(req,res)=>{
+app.get('*',(req,res)=>{
     res.sendFile(path.resolve(__dirname,"index.html"));
 })
+
 var server=app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
 }); 
